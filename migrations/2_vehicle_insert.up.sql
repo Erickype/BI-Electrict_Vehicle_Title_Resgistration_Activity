9702532114aa7c61bdd395740c1eb0c1a7255cfc
Erickype <erickype@hotmail.com>
2023-10-03 15:16:57 -0500

    Vehicle insertion

--- migrations/2_vehicle_insert.up.sql
+++ migrations/2_vehicle_insert.up.sql
@@ -36,3 +36,25 @@ from tmp_vehicle_model
 inner join db.vehicle_make VMake on tmp_vehicle_model."Make" = VMake.name;
 
 drop table tmp_vehicle_model;
+
+-- Vehicle insert
+create temp table tmp_vehicle as
+select distinct on (PV."DOL Vehicle ID")
+    PV,"DOL Vehicle ID",
+    PV."VIN (1-10)",
+    VT.id as VType,
+    VM.id as Vmodel,
+    PV."Model Year",
+    PV."Electric Range",
+    PV."Base MSRP"
+from public."Electric_Vehicle_Title_and_Registration_Activity" PV
+inner join db.vehicle_type VT on PV."Clean Alternative Fuel Vehicle Type" = VT.name
+inner join db.vehicle_model VM on PV."Model" = VM.name
+order by "DOL Vehicle ID";
+
+insert into db.vehicle
+(dol_id, vin, type, model, model_year, electric_range, base_msrp)
+select "DOL Vehicle ID", "VIN (1-10)", VType, Vmodel, "Model Year", "Electric Range", "Base MSRP"
+from tmp_vehicle TV;
+
+drop table tmp_vehicle;
