c0589b84ce89b654f0c977629b4e45f269544156
Erickype <erickype@hotmail.com>
2023-10-02 20:05:39 -0500

    Added constraints vehicle tables migration file

--- migrations/1_vehicle.up.sql
+++ migrations/1_vehicle.up.sql
@@ -15,10 +15,26 @@ create table db.vehicle_model(
 );
 
 create table db.vehicle(
-    dol_vehicle_id integer primary key,
+    dol_id integer primary key,
     vin text,
-    vehicle_type integer,
+    type integer,
     model integer,
     model_year integer,
-    electric_range numeric
-)
+    electric_range numeric,
+    base_msrp numeric
+);
+
+ALTER TABLE db.vehicle_model
+    ADD CONSTRAINT fk_vehicle_make
+        FOREIGN KEY (vehicle_make)
+            REFERENCES db.vehicle_make (id);
+
+ALTER TABLE db.vehicle
+    ADD CONSTRAINT fk_vehicle_model
+        FOREIGN KEY (model)
+            REFERENCES db.vehicle_model (id);
+
+ALTER TABLE db.vehicle
+    ADD CONSTRAINT fk_vehicle_type
+        FOREIGN KEY (type)
+            REFERENCES db.vehicle_type (id);
