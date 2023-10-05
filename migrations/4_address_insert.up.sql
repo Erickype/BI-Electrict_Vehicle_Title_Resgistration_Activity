a2fdf76d0a114a3ec5735807523e79e03c29ea79
Erickype <erickype@hotmail.com>
2023-10-05 17:10:51 -0500

    Data address insert for not null postal codes

--- migrations/4_address_insert.up.sql
+++ migrations/4_address_insert.up.sql
@@ -14,3 +14,33 @@ insert into db.state (name)
 select distinct "State of Residence"
 from public."Electric_Vehicle_Title_and_Registration_Activity"
 order by "State of Residence";
+
+                --Address--
+--not nulls
+create temp table tmp_address as
+select distinct on ("Postal Code")
+    "County",
+    "City",
+    "State of Residence",
+    "Postal Code"
+from public."Electric_Vehicle_Title_and_Registration_Activity" EVTaRA
+where "Postal Code" is not null
+order by "Postal Code";
+
+insert into db.address (postalcode, country, city, state)
+select
+    "Postal Code",
+    C.id as country,
+    cy.id as city,
+    s.id as state
+from tmp_address TAdd
+left join db.country C on
+    TAdd."County" = C.name or (TAdd."County" isnull AND C.name isnull)
+left join db.city cy on
+    TAdd."City" = cy.name or (TAdd."City" isnull AND cy.name isnull)
+left join db.state s on
+    TAdd."State of Residence" = s.name or
+    (TAdd."State of Residence" isnull AND s.name isnull)
+order by "Postal Code";
+
+drop table tmp_address;
