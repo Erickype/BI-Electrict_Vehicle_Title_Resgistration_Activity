bbe4bbd1f5631de79a64d1a50f9e0fa374d14055
Erickype <erickype@hotmail.com>
2023-10-05 13:38:03 -0500

    State insert table

--- migrations/4_address_insert.up.sql
+++ migrations/4_address_insert.up.sql
@@ -16,3 +16,19 @@ from tmp_city TC
 left join db.country C on
     TC."County" = C.name OR (TC."County" IS NULL AND C.name IS NULL)
 order by C.id;
+
+-- state
+create temp table tmp_state as
+select distinct on ("State of Residence")
+    "City", "State of Residence"
+from public."Electric_Vehicle_Title_and_Registration_Activity"
+order by "State of Residence";
+
+insert into db.state (city, name)
+select
+    C.id,
+    TS."State of Residence"
+from tmp_state TS
+left join db.city C on
+    TS."City" = C.name or (TS."City" isnull and C.name isnull)
+order by Ts."State of Residence";
