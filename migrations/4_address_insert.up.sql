e7eaafd7b595b00011c1fd0ffd8d2a0dd931d625
Erickype <erickype@hotmail.com>
2023-10-05 13:23:16 -0500

    City insert table

--- migrations/4_address_insert.up.sql
+++ migrations/4_address_insert.up.sql
@@ -9,8 +9,10 @@ select distinct on ("City")
     "County", "City"
 from public."Electric_Vehicle_Title_and_Registration_Activity";
 
+insert into db.city (country, name)
 select
-    C.id, C.name, TC."City"
+    C.id, TC."City"
 from tmp_city TC
 left join db.country C on
-    TC."County" = C.name OR (TC."County" IS NULL AND C.name IS NULL);
+    TC."County" = C.name OR (TC."County" IS NULL AND C.name IS NULL)
+order by C.id;
