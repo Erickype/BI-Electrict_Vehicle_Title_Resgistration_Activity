77e88729dbf292741c53193bef16bf2e59d19145
Erickype <erickype@hotmail.com>
2023-10-05 16:42:47 -0500

    Data address modeling fixed, insert city

--- migrations/4_address_insert.up.sql
+++ migrations/4_address_insert.up.sql
@@ -4,18 +4,10 @@ select distinct "County"
 from public."Electric_Vehicle_Title_and_Registration_Activity";
 
 -- City
-create temp table tmp_city as
-select distinct on ("City")
-    "County", "City"
-from public."Electric_Vehicle_Title_and_Registration_Activity";
-
-insert into db.city (country, name)
-select
-    C.id, TC."City"
-from tmp_city TC
-left join db.country C on
-    TC."County" = C.name OR (TC."County" IS NULL AND C.name IS NULL)
-order by C.id;
+insert into db.city (name)
+select distinct "City"
+from public."Electric_Vehicle_Title_and_Registration_Activity"
+order by "City";
 
 -- state
 create temp table tmp_state as
