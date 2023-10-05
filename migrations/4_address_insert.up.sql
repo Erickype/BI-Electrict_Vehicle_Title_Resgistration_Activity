00629e51c4000d708b801242fffb102307695117
Erick Carrasco <erickype@hotmail.com>
2023-10-05 17:48:32 +0000

    City data insert (needs fix)

--- migrations/4_address_insert.up.sql
+++ migrations/4_address_insert.up.sql
@@ -1,3 +1,16 @@
+-- Country
 insert into db.country (name)
 select distinct "County"
 from public."Electric_Vehicle_Title_and_Registration_Activity";
+
+-- City
+create temp table tmp_city as
+select distinct on ("City")
+    "County", "City"
+from public."Electric_Vehicle_Title_and_Registration_Activity";
+
+select
+    C.id, C.name, TC."City"
+from tmp_city TC
+left join db.country C on
+    TC."County" = C.name OR (TC."County" IS NULL AND C.name IS NULL);
