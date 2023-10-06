1513f33832313110b4279dd2e11451fd49a150c2
Erickype <erickype@hotmail.com>
2023-10-06 17:32:20 -0500

    Transactions initial fields insert

--- migrations/6_tran_insert.up.sql
+++ migrations/6_tran_insert.up.sql
@@ -31,3 +31,34 @@ select
     "Postal Code"
 from public."Electric_Vehicle_Title_and_Registration_Activity"
 order by "DOL Vehicle ID";
+
+insert into db.transactions
+    (dol_vehicle_id,
+     vehicle_primary_use,
+     odometer_reading,
+     odometer_code,
+     new_or_used,
+     sale_price,
+     sale_date,
+     transaction_type,
+     dol_transaction_date,
+     postal_code)
+select
+    TT."DOL Vehicle ID",
+    VH.id as VehicleUse,
+    TT."Odometer Reading",
+    OC.id as OdometerCode,
+    TT."New or Used Vehicle",
+    TT."Sale Price",
+    to_date(TT."Sale Date",'Month DD YYYY') as SaleDate,
+    TTy.id as TransType,
+    to_date(TT."DOL Transaction Date", 'Month DD YYYY') as DolTransationDate,
+    TT."Postal Code"
+from tmp_tran TT
+left join db.vehicle_uses VH on
+    TT."Vehicle Primary Use" = VH.name
+left join db.odometer_codes OC on
+    TT."Odometer Code" = OC.name
+left join db.transaction_types TTy on
+    TT."Transaction Type" = TTy.name
+order by TT."DOL Vehicle ID";
