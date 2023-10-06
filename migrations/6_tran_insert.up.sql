bbdbd7004e8edd44417b082e3310c980015b69b8
Erickype <erickype@hotmail.com>
2023-10-06 16:48:07 -0500

    Transaction types insert

--- migrations/6_tran_insert.up.sql
+++ migrations/6_tran_insert.up.sql
@@ -9,3 +9,9 @@ insert into db.odometer_codes (name)
 select distinct "Odometer Code"
 from public."Electric_Vehicle_Title_and_Registration_Activity"
 order by "Odometer Code";
+
+-- Transaction types
+insert into db.transaction_types (name)
+select distinct "Transaction Type"
+from public."Electric_Vehicle_Title_and_Registration_Activity"
+order by "Transaction Type";
