541525d7be100a7d5fa5f9401ff77a8e7c5e5593
Erickype <erickype@hotmail.com>
2023-10-06 17:00:51 -0500

    Transactions temp table

--- migrations/6_tran_insert.up.sql
+++ migrations/6_tran_insert.up.sql
@@ -15,3 +15,19 @@ insert into db.transaction_types (name)
 select distinct "Transaction Type"
 from public."Electric_Vehicle_Title_and_Registration_Activity"
 order by "Transaction Type";
+
+-- Transactions
+create temp table tmp_tran as
+select
+    "DOL Vehicle ID",
+    "Vehicle Primary Use",
+    "Odometer Reading",
+    "Odometer Code",
+    "New or Used Vehicle",
+    "Sale Price",
+    "Sale Date",
+    "Transaction Type",
+    "DOL Transaction Date",
+    "Postal Code"
+from public."Electric_Vehicle_Title_and_Registration_Activity"
+order by "DOL Vehicle ID";
