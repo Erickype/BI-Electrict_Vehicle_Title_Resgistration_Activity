facb69d791be4fb24a87525d30aade9ebf724c79
Erickype <erickype@hotmail.com>
2023-10-07 15:27:10 -0500

    Legislation requirement code fk added

--- migrations/6_trans_final.up.sql
+++ migrations/6_trans_final.up.sql
@@ -1,33 +1,38 @@
-create table Exception_Eligibility_Codes(
+create table db.Exception_Eligibility_Codes(
     id serial primary key,
     name text
 );
 
-create table Legislation(
+create table db.Legislation(
     id serial primary key,
     name text
 );
 
-create table Legislation_Requirement_Code(
+create table db.Legislation_Requirement_Code(
     id serial primary key,
     legislation integer,
     name text
 );
 
-create table Legislation_Requirement_Details_Code(
+create table db.Legislation_Requirement_Details_Code(
      id serial primary key,
      legislation integer,
      name text
 );
 
-create table Transaction_Legislation(
+create table db.Transaction_Legislation(
     id serial primary key,
     legislation integer,
     exception_eligibility integer
 );
 
-create table Transaction_Legislation_Detail(
+create table db.Transaction_Legislation_Detail(
     transaction_legislation integer,
     legislation_requirement_code integer,
     legislation_requirement_details_code integer
 );
+
+alter table db.Legislation_Requirement_Code
+add constraint fk_legislation
+foreign key (legislation)
+references db.legislation (id);
