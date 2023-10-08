61194c04ee55d1436b2a7317f81ab1ba5911b57d
Erickype <erickype@hotmail.com>
2023-10-08 14:05:47 -0500

    TransactionLegislationDetail Price/Value Requirement insert

--- migrations/table_insert/5_legis_details_insert.up.sql
+++ migrations/table_insert/5_legis_details_insert.up.sql
@@ -92,3 +92,51 @@ where legislation = (
     where L.name = '2019 HB 2042'
 )
 order by TD.id;
+
+drop table pg_temp.tmp_trans_detail;
+
+-- 2019 Sale Date Requirement
+create temp table tmp_trans_detail as
+select
+    row_number() over () as id,
+    (
+        select id
+        from db.legislation_requirement_code LRC
+        where LRC.name = 'S ale Price/Value Requirement'
+    ) as LRC,
+    "Meets 2019 HB 2042 Sale Date Requirement" as LRV,
+    (
+        select id
+        from db.legislation_requirement_details_code LRDC
+        where LRDC.name = 'Sale Price/Value Requirement'
+    ) as LRDC,
+    LRDV.id as LRDV
+from public."Electric_Vehicle_Title_and_Registration_Activity" EVTaRA
+left join db.legislation_requirement_details_values LRDV on
+    EVTaRA."2019 HB 2042: Sale Price/Value Requirement" = LRDV.name or
+    (EVTaRA."2019 HB 2042: Sale Price/Value Requirement" isnull AND LRDV.name isnull)
+order by "DOL Vehicle ID";
+
+insert into db.transaction_legislation_detail
+(
+    transaction_legislation,
+    legislation_requirement_code,
+    legislation_requirement_value,
+    legislation_requirement_details_code,
+    legislation_requirement_details_value
+)
+select
+    TL.id,
+    TD.LRC,
+    TD.LRV::boolean,
+    TD.LRDC,
+    TD.LRDV
+from tmp_trans_detail TD
+left join db.transaction_legislation TL on
+    TD.id = TL.transaction
+where legislation = (
+    select L.id
+    from db.legislation L
+    where L.name = '2019 HB 2042'
+)
+order by TD.id;
