dce650a3ea5d036368f28120b6b0759010358be6
Erickype <erickype@hotmail.com>
2023-10-17 15:25:18 -0500

    Dimension table Vehicle type etl

--- migrations/olap/table_insert/1_dimensions.up.sql
+++ migrations/olap/table_insert/1_dimensions.up.sql
@@ -1,10 +1,8 @@
 -- DimVehicleType
-insert into dw.dimvehicletype (id, name)
 select id, name
 from db.vehicle_type;
 
 -- DimDate
-insert into dw.dimdate(date)
 select distinct dol_transaction_date
 from db.transactions
 order by dol_transaction_date;
