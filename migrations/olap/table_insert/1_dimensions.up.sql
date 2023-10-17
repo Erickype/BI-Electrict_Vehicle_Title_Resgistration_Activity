6f37e063630c47a73304f251523027da01c5323f
Erickype <erickype@hotmail.com>
2023-10-17 14:56:46 -0500

    OLAP dimension date insert table

--- migrations/olap/table_insert/1_dimensions.up.sql
+++ migrations/olap/table_insert/1_dimensions.up.sql
@@ -2,3 +2,9 @@
 insert into dw.dimvehicletype (id, name)
 select id, name
 from db.vehicle_type;
+
+-- DimDate
+insert into dw.dimdate(date)
+select distinct dol_transaction_date
+from db.transactions
+order by dol_transaction_date;
